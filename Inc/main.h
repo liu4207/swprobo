78ce6ea6ffcf66d085a5676ee87e857214d9996e
liu4207 <2323453274@qq.com>
2025-05-05 23:42:49 +0800

    test

---
+++ Inc/main.h
@@ -0,0 +1,119 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define PUMP_I_Pin GPIO_PIN_2
+#define PUMP_I_GPIO_Port GPIOE
+#define FAN_PWM_Pin GPIO_PIN_3
+#define FAN_PWM_GPIO_Port GPIOE
+#define FAN_FB_Pin GPIO_PIN_4
+#define FAN_FB_GPIO_Port GPIOE
+#define MAGNET_Pin GPIO_PIN_5
+#define MAGNET_GPIO_Port GPIOE
+#define BAT0_Pin GPIO_PIN_4
+#define BAT0_GPIO_Port GPIOC
+#define BMQ_R1_Pin GPIO_PIN_0
+#define BMQ_R1_GPIO_Port GPIOB
+#define RB_I_Pin GPIO_PIN_9
+#define RB_I_GPIO_Port GPIOE
+#define IR_SD_Pin GPIO_PIN_10
+#define IR_SD_GPIO_Port GPIOE
+#define IR_BT_R_Pin GPIO_PIN_11
+#define IR_BT_R_GPIO_Port GPIOE
+#define IR_CC_R1_Pin GPIO_PIN_12
+#define IR_CC_R1_GPIO_Port GPIOE
+#define IRHIT_L_Pin GPIO_PIN_12
+#define IRHIT_L_GPIO_Port GPIOB
+#define user_led_Pin GPIO_PIN_15
+#define user_led_GPIO_Port GPIOB
+#define IR_CC_R2_Pin GPIO_PIN_10
+#define IR_CC_R2_GPIO_Port GPIOD
+#define IR_CC_L2_Pin GPIO_PIN_11
+#define IR_CC_L2_GPIO_Port GPIOD
+#define LWI2_Pin GPIO_PIN_12
+#define LWI2_GPIO_Port GPIOD
+#define LWI1_Pin GPIO_PIN_13
+#define LWI1_GPIO_Port GPIOD
+#define RWI2_Pin GPIO_PIN_14
+#define RWI2_GPIO_Port GPIOD
+#define RWI1_Pin GPIO_PIN_15
+#define RWI1_GPIO_Port GPIOD
+#define IRHIT_R_Pin GPIO_PIN_15
+#define IRHIT_R_GPIO_Port GPIOA
+#define BMQ_L1_Pin GPIO_PIN_0
+#define BMQ_L1_GPIO_Port GPIOD
+#define IR_CC_L1_Pin GPIO_PIN_3
+#define IR_CC_L1_GPIO_Port GPIOB
+#define IR_BT_F_Pin GPIO_PIN_4
+#define IR_BT_F_GPIO_Port GPIOB
+#define IR_BT_L_Pin GPIO_PIN_5
+#define IR_BT_L_GPIO_Port GPIOB
+#define LB_I_Pin GPIO_PIN_6
+#define LB_I_GPIO_Port GPIOB
+#define user_button_Pin GPIO_PIN_0
+#define user_button_GPIO_Port GPIOE
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ MDK-ARM/motor_control.h
@@ -0,0 +1,32 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+#include "main.h"
+
+// L298N控制引脚（假设连接到这些GPIO，根据实际硬件连接调整）
+#define L298N_INPUT1_GPIO_PORT GPIOD  // 假设连接到PD13，对应LWI1
+#define L298N_INPUT1_PIN GPIO_PIN_13
+#define L298N_INPUT2_GPIO_PORT GPIOD  // 假设连接到PD12，对应LWI2
+#define L298N_INPUT2_PIN GPIO_PIN_12
+#define L298N_INPUT3_GPIO_PORT GPIOD  // 假设连接到PD14，对应RWI1
+#define L298N_INPUT3_PIN GPIO_PIN_14
+#define L298N_INPUT4_GPIO_PORT GPIOD  // 假设连接到PD15，对应RWI2
+#define L298N_INPUT4_PIN GPIO_PIN_15
+
+// TIM3相关定义
+extern TIM_HandleTypeDef htim3;
+#define MOTOR_PWM_TIMER htim3
+#define MOTOR_PWM_CHANNEL1 TIM_CHANNEL_3
+#define MOTOR_PWM_CHANNEL2 TIM_CHANNEL_4
+
+// 函数声明
+void Motor_Init(void);
+void Motor_SetSpeed(uint8_t speed);
+void Motor_Forward(void);
+void Motor_Backward(void);
+void Motor_Stop(void);
+void Motor_SoftStart(uint8_t target_speed, uint16_t duration);
+void Motor_Brake(void);
+
+
+#endif
